ea6147438a79b7a577b3fe81f599fc108bfed56a
wellyton-xs <wellyton.offcer@gmail.com>
2024-04-25 20:46:15 -0300

    day 3 learning C
    at this day I learn:
    	* change decimal precision
    	* size of data types
    	* more real life examples

---
+++ src/setDecimalPrecision.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+	float num = 5; // outputs 5.000000
+	printf("%f\n", num);
+
+	// you can set the number of the digits.
+
+	printf("%.1f\n", num);
+	printf("%.2f\n", num);
+	printf("%.3f", num);
+	return 0;
+}

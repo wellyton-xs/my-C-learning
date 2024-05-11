78e7f124d92536df43b8b151ec387ccc75233c88
wellyton-xs <wellyton.offcer@gmail.com>
2024-05-10 23:52:42 -0300

    day 4 learning c
    	type conversion

---
+++ src/typeConversion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+	// implicit conversion
+	
+	// the compiler will convert this int to a float
+	float numberNine = 9;
+
+	printf("%f", numberNine);
+
+	// another example
+	int ninePointNineNine = 9.99;
+	
+	printf("%d", ninePointNineNine); 
+
+	// explicit conversion
+
+	int five = 5;
+	int two = 2;
+	
+	float sum = (float) five / two;
+	
+	printf("%f", sum); 
+}

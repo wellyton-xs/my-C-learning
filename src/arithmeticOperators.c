76ad29acc7079a8c270e9514281dbe23a850bf89
wellyton-xs <wellyton.offcer@gmail.com>
2024-05-11 19:29:53 -0300

    day 5 learning C
    	constants
    	arithmetic operators

---
+++ src/arithmeticOperators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int main(){
+
+	int sum1 = 100 + 50;
+	int sum2 = sum1 + 250;
+	int sum3 = sum2 + sum2;
+
+	printf("value of sums: 1; %d 2; %d 3; %d\n", sum1, sum2, sum3);
+
+	/*/ arithmetic  operators:
+
+	[+] addition ( x + y )
+	[-] subtaction ( x - y )
+	[*] multiplication ( x * y  )
+	[/] division ( x / y )
+	[%] modulus ( x % y  )
+	[++] increment ( x + 1  )
+	[--] decrement ( x - 1 )
+	
+
+	/*/
+	
+	return 0;
+}
---
+++ src/constants.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+	const int MYBIRTH = 2006;
+	printf("my birthday is: %d\n", MYBIRTH);
+	
+	return 0;
+}

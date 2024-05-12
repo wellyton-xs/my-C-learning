949f4322e683413d395e5ed752f7f777cfbe6e06
wellyton-xs <wellyton.offcer@gmail.com>
2024-05-12 19:49:55 -0300

    day 6 learning C
    	assignment operators
    	new script

---
+++ src/assignmentOperators.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+	int x = 10; // assignment "="
+	x += 5; // assignment "+="
+
+	/*/
+
+		operator:	example:
+		[=]  		( x = 5 )
+		[+=] 		( x += 5 )
+		[-=] 		( x -= 5 )
+		[*=] 		( x *= 5 )
+		[/=] 		( x /= 5 )
+		[%=] 		( x %= 5 )
+		[&=] 		( x &= 5 )
+		[|=]		( x |= 5 )
+		[^=]		( x ^= 5 )
+		[>>=]		( x >>= 5 )
+		[<<=]		( x <<= 5 )
+	/*/
+	
+	return 0;
+}
---
+++ src/test.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+
+int main(){
+
+	printf("this is a test of create script!\n");
+	
+	return 0;
+}
+
+
